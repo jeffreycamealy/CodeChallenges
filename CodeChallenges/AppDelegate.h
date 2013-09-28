686353d2b3ffb14d0d17f8f2fbd721e2a7213845
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-09-28 15:30:55 -0500

    Initial Commit

---
+++ CodeChallenges/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CodeChallenges
+//
+//  Created by Jeffrey Camealy on 9/28/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CodeChallenges/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CodeChallenges
+//
+//  Created by Jeffrey Camealy on 9/28/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
